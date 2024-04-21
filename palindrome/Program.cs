22837124238ca5bd2163796b8a3917dc2433ca15
felipe <felype08.aravena@gmail.com>
2024-04-21 12:37:00 +1200

    test

---
+++ palindrome/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string[] array =
+            {
+                "abcba2",
+                "abcde",
+                "Mr owl ate my metal worm",
+                "Never Odd Or Even",
+                "Never Even Or Odd"
+            };
+
+        foreach (string value in array)
+        {
+            if (IsPalindrome(value))
+            {
+                Console.WriteLine($"'{value}' Is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"'{value}' Is not a palindrome.");
+            }
+        }
+
+    }
+
+    static bool IsPalindrome(string input)
+    {
+        //Convert input to lowercase and remove non-alphabetic characters
+        string converInput = ConverText(input);
+
+        //Check if input is a palindrome
+        int i = 0;
+        int j = converInput.Length - 1;
+
+        //runs through the flow
+        while (i < j)
+        {
+            if (converInput[i] != converInput[j])
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    static string ConverText(string input)
+    {
+        //Convert to lowercase and remove non-alphabetic characters
+        string normalized = "";
+        foreach (char c in input.ToLower())
+        {
+            if (char.IsLetter(c))
+            {
+                normalized += c;
+            }
+        }
+        return normalized;
+    }
+}
